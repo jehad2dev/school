e24ef47f111b4a155bc263d9336485e3b96a0b9e
Morreels Michael <morreelsm@gmail.com>
2012-08-15 15:16:37 +0200

    Update .sql

--- _docs/school.sql
+++ _docs/school.sql
@@ -1,15 +1,14 @@
 -- --------------------------------------------------------
 -- Host:                         127.0.0.1
--- Server version:               5.5.8-log - MySQL Community Server (GPL)
--- Server OS:                    Win32
--- HeidiSQL version:             7.0.0.4160
--- Date/time:                    2012-08-03 14:29:24
+-- Server version:               5.5.16-log - MySQL Community Server (GPL)
+-- Server OS:                    Win64
+-- HeidiSQL version:             7.0.0.4053
+-- Date/time:                    2012-08-15 15:16:11
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET NAMES utf8 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40014 SET FOREIGN_KEY_CHECKS=0 */;
 
 -- Dumping database structure for school
 CREATE DATABASE IF NOT EXISTS `school` /*!40100 DEFAULT CHARACTER SET latin1 */;
@@ -26,6 +25,17 @@ CREATE TABLE IF NOT EXISTS `competences` (
   `discipline_id` tinyint(4) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Data exporting was unselected.
+
+
+-- Dumping structure for table school.competences_sheets
+CREATE TABLE IF NOT EXISTS `competences_sheets` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `sheet_id` smallint(6) NOT NULL DEFAULT '0',
+  `competence_id` smallint(6) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
 
 -- Data exporting was unselected.
 
@@ -70,29 +80,48 @@ CREATE TABLE IF NOT EXISTS `materials` (
 -- Data exporting was unselected.
 
 
+-- Dumping structure for table school.materials_sheets
+CREATE TABLE IF NOT EXISTS `materials_sheets` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `sheet_id` smallint(6) NOT NULL DEFAULT '0',
+  `material_id` int(10) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Data exporting was unselected.
+
+
+-- Dumping structure for table school.projects
+CREATE TABLE IF NOT EXISTS `projects` (
+  `id` tinyint(4) NOT NULL AUTO_INCREMENT,
+  `name` varchar(99) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
+
+-- Data exporting was unselected.
+
+
 -- Dumping structure for table school.sheets
 CREATE TABLE IF NOT EXISTS `sheets` (
   `id` smallint(6) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
   `progress` text,
-  `development` text NOT NULL,
-  `teacher_task` text NOT NULL,
+  `comment` text NOT NULL,
+  `challenge` text NOT NULL,
   `target` text NOT NULL,
-  `learningAxis_id` smallint(4) NOT NULL,
-  `evaluation_id` tinyint(1) unsigned zerofill DEFAULT NULL,
-  `target_competence` smallint(4) unsigned zerofill NOT NULL,
+  `learning_axis_id` smallint(4) NOT NULL,
+  `evaluation_id` tinyint(4) NOT NULL,
+  `competence_id` smallint(4) NOT NULL COMMENT 'Id for the target competence',
   `discipline_id` tinyint(4) NOT NULL,
-  `used_competence_list_id` text COMMENT 'List of competence used (in serialized array)',
-  `material_list_id` text COMMENT 'List of material used (in serialized array)',
   `date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `date_last_modified` timestamp NULL DEFAULT NULL,
+  `project_id` tinyint(4) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `fk_preparation_learningAxis1` (`learningAxis_id`),
+  KEY `fk_preparation_learningAxis1` (`learning_axis_id`),
   KEY `fk_preparation_evaluation1` (`evaluation_id`),
-  KEY `fk_preparation_competence2` (`target_competence`)
+  KEY `fk_preparation_competence2` (`competence_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- Data exporting was unselected.
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40014 SET FOREIGN_KEY_CHECKS=1 */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
